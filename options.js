40517217573aae320eda02214094dbbfb875b723
averageatar <81802196+averageatar@users.noreply.github.com>
2021-04-03 19:44:52 +0800

    Add files via upload

--- options.js
+++ options.js
@@ -1,37 +1,3 @@
-const DEFAULT_OPTIONS_FILE = "BatsuOptions.txt"; //where the options are saved to
+var input = document.getElementById("sites").value;
 
-function getElement(id) { return document.getElementById(id); } //get the websites
-
-function isTrue(str) { return /^true$/i.test(str); } //returns true if success
-
-//Set up JQuery UI widgets 
-$("#saveOptions").button();
-$("#saveOptions").click({ closeOptions: false }, saveOptions);
-$("#saveOptionsClose").button();
-$("#saveOptionsClose").click({ closeOptions: true }, saveOptions);
-
-// Save options to local storage (returns true if success)
-
-function saveOptions(event) {
-	//log("saveOptions");
-
-    let blockURL = $(`#blockURL${set}`).val();
-    
-    if (blockURL != DEFAULT_BLOCK_URL && blockURL != DELAYED_BLOCK_URL
-            && !getParsedURL(blockURL).page) {
-        $("#tabs").tabs("option", "active", (set - 1));
-        $(`#blockURL${set}`).focus();
-        $("#alertBadBlockURL").dialog("open");
-        return false;   
-    }
-}
-
-function closeOptions() {
-	// Request tab close
-	browser.runtime.sendMessage({ type: "close" });
-}
-
-function othername() {
-    var input = document.getElementById("userInput").value;
-    alert(input);
-}
+alert(input);
