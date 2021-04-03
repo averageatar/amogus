d445c18a6a056b96853ca84fbb3fe2f56ad9faa0
mumslayer69 <81803537+mumslayer69@users.noreply.github.com>
2021-04-03 21:10:03 +1100

    Add files via upload

---
+++ hentaiblocker/background.js
@@ -0,0 +1,13 @@
+const BatsuBlock = {
+    init: ( ) => {
+    console.log("Initiate Batsu Block");
+
+    }
+}
+
+BatsuBlock.init();
+
+//for (let i = 0; i < Infinity; i++) {}
+
+//document.body.style.border = "10px solid red";
+//window.alert("OwO");
